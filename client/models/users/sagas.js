bbf20385792ac53d4af0d34ef91c655b44da566b
Oskar Szura <oskar.szura@gmail.com>
2020-05-20 07:44:03 +0200

    CODA-73 - Add user-post relation

--- client/models/users/sagas.js
+++ client/models/users/sagas.js
@@ -30,8 +30,7 @@ export function* onFetchUser(): Iterable<any> {
     yield put(
       alertsActions.addAlert(
         'Fetching user data failed',
-        alertsConstants.ALERT_TYPE_ERROR,
-        new Date()
+        alertsConstants.ALERT_TYPE_ERROR
       )
     );
   }
