3e3452f299dd8d20d640e93e8770749120248178
Oskar Szura <oskar.szura@gmail.com>
2020-05-22 07:55:37 +0200

    CODA-76 - Admin submenu (#6)

--- client/modules/Admin/Admin.jsx
+++ client/modules/Admin/Admin.jsx
@@ -1,14 +1,11 @@
 // @flow
 import React from 'react';
 import { withRouter } from 'react-router';
-import { Link } from 'graphen';
 
 function Posts() {
   return (
     <div className="gc-panel gc-panel--separator gm-spacing-bl">
-      <div className="gc-panel__content">
-        <Link link="/admin/new-post">Add post</Link>
-      </div>
+      <div className="gc-panel__content" />
     </div>
   );
 }
