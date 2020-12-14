f0119eb9f7135ba2d6303387f5f26c928c568fb3
Oskar Szura <oskar.szura@gmail.com>
2020-12-14 11:43:27 +0100

    CODA-130 - Clean architecture cleanup

--- handlers/render.go
+++ handlers/render.go
@@ -2,8 +2,8 @@ package handlers
 
 import (
 	"github.com/coda-it/goutils/logger"
-	"github.com/coda-it/gowebapp/models/page"
 	userServices "github.com/coda-it/gowebapp/helpers/user"
+	"github.com/coda-it/gowebapp/models/page"
 	"github.com/coda-it/gowebapp/utils"
 	"github.com/coda-it/gowebserver/session"
 	"html/template"
