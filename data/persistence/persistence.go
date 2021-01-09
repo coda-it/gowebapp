40792e63e31bbf443dbbbca2d1f71116dd0145b4
Oskar Szura <oskar.szura@gmail.com>
2021-01-09 18:32:35 +0100

    CODA-133 - Application framework

--- data/persistence/persistence.go
+++ data/persistence/persistence.go
@@ -4,12 +4,6 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-// IPersistance - interface for user settings and general purpose storage
-type IPersistance interface {
-	GetCollection(string) *mgo.Collection
-	DropDatabase() error
-}
-
 // Persistance - data source keeping system state and user data
 type Persistance struct {
 	session *mgo.Session
--- utils/data.go
+++ utils/data.go
@@ -2,16 +2,16 @@ package utils
 
 import (
 	"errors"
+	"github.com/coda-it/goappframe"
 	"github.com/coda-it/gowebapp/constants"
-	"github.com/coda-it/gowebapp/data/persistence"
 	"github.com/coda-it/gowebserver/store"
 )
 
 // GetPersistence - return persistence data source and handler error if unsuccessful
-func GetPersistence(s store.IStore) (persistence.IPersistance, error) {
+func GetPersistence(s store.IStore) (goappframe.IPersistance, error) {
 	dataSource := s.GetDataSource(constants.PersistenceDataKey)
 
-	p, ok := dataSource.(persistence.IPersistance)
+	p, ok := dataSource.(goappframe.IPersistance)
 	if !ok {
 		return nil, errors.New("unsupported data source")
 	}
