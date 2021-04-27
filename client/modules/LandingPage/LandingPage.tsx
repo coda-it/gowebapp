f767385de127a817cb63857f7b6c4906b817a76f
Bartosz Szura <bartosz.szura100@gmail.com>
2021-04-27 20:39:23 +0200

    add button

--- client/modules/LandingPage/LandingPage.tsx
+++ client/modules/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import { Button } from 'graphen';
 
 function LandingPage() {
 return (
@@ -8,16 +9,22 @@ return (
         <article className="gc-panel__content">
         <div className="gc-input gc-input--full">
             {/* eslint-disable jsx-a11y/label-has-associated-control */}
-            <label htmlFor="post-title" className="gc-input__label">
-              Title
+            <label htmlFor="landing-page-title" className="gc-input__label">
+              Landing Page Title
             </label>
             {/* eslint-enable jsx-a11y/label-has-associated-control */}
             <input
               id="landing page"
-              className="gc-input__field tst-post-editor-title"
+              className="gc-input__field tst-landing-page-title"
             />
             </div>
             </article>
+             <Button
+            className="gc-btn--primary tst-post-editor-add gc-btn"
+            onClick={() => console.log('click')}
+          >
+            Add
+          </Button>
        </div>
        
        );
