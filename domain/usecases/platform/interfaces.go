487545eaf6703ca3e00f49075544c6aa16f52cd5
Oskar Szura <oskar.szura@gmail.com>
2024-05-04 07:59:05 +0200

    CODA-311 - Refactor get application by request

--- domain/usecases/platform/interfaces.go
+++ domain/usecases/platform/interfaces.go
@@ -1,9 +1,7 @@
 package platform
 
 import (
-	"github.com/coda-it/goappframe/config"
 	platformModel "github.com/coda-it/gowebapp/domain/models/platform"
-	"net/http"
 )
 
 // IRepository - platform repository interface
@@ -12,5 +10,4 @@ type IRepository interface {
 	Add(c platformModel.Config) error
 	Update(c platformModel.Config) error
 	Fetch(appID string) (platformModel.Config, error)
-	GetApplicationByDomain(cnf config.Config, r *http.Request) config.App
 }
