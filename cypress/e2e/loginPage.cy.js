f4ad9e2d335124efc6e680556d8a85893824124b
Oskar Szura <oskar.szura@gmail.com>
2024-05-09 08:22:00 +0200

    test

--- cypress/e2e/loginPage.cy.js
+++ cypress/e2e/loginPage.cy.js
@@ -7,7 +7,7 @@ describe('Login page', () => {
   it('should register successfully', () => {
     cy.visit('http://localhost:3000/login/register');
     cy.get('.tst-email').type('admin@localhost.pl');
-    cy.get('.tst-password').type('admin');
+    cy.get('.tst-password').type('admin'); 
     cy.get('.tst-register').click();
   });
 
