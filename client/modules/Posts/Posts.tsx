c35f70c205f9c26850a0a9a3be39aaacc6fdccae
Oskar Szura <oskar.szura@gmail.com>
2024-05-19 08:38:30 +0200

    CODA-321 - UI micro adjustments

--- client/modules/Posts/Posts.tsx
+++ client/modules/Posts/Posts.tsx
@@ -1,63 +1,87 @@
-import _ from 'lodash';
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router';
-import { constants, Link, Image } from 'graphen';
+import {
+  constants,
+  Link,
+  Image,
+  Panel,
+  PanelContent,
+  PanelTitle,
+  Flex,
+  FlexItem,
+  Card,
+} from 'graphen';
 import * as types from './types';
 
-function Posts(props: types.Props) {
+function Posts(
+  props: types.Props = {
+    posts: [],
+    loadPosts: () => {},
+    loadCategories: () => {},
+  }
+) {
   const { isAdmin, loadPosts, posts, user, category, loadCategories } = props;
 
   useEffect(() => {
     loadPosts(user);
     loadCategories();
-  }, [loadPosts]);
+  }, [loadPosts, loadCategories]);
 
   return (
-    <div className="gc-panel">
-      <div className="gc-panel__title">
-        {`Category: ${category ? category.name : 'All'}`}
-      </div>
-      <div className="gc-panel__content gc-flex gc-flex--wrap tst-posts">
-        {/* @ts-ignore */}
-        {_.map(posts, ({ id, title, image }, key) => {
-          const link = `/admin/posts/edit/${id}`;
-          const editButton = isAdmin ? (
-            <a
-              className={`gc-btn gc-btn--small gc-btn--primary tst-post-edit-${key}`}
-              href={link}
-            >
-              Edit
-            </a>
-          ) : null;
+    <Panel>
+      <PanelTitle>{`Category: ${category ? category.name : 'All'}`}</PanelTitle>
+      <PanelContent className="gc-flex gc-flex--wrap tst-posts">
+        <Flex wrap="wrap">
+          {posts.map(({ id, title, image }, key) => {
+            const link = `/admin/posts/edit/${id}`;
+            const editButton = isAdmin ? (
+              <a
+                className={`gc-btn gc-btn--small gc-btn--primary tst-post-edit-${key}`}
+                href={link}
+              >
+                Edit
+              </a>
+            ) : null;
 
-          return (
-            <div
-              key={key}
-              className={`gc-flex__item gc-card gc-card--gradient gc-panel gm-spacing-l tst-post-${key}`}
-            >
-              <div className={`gc-panel__title tst-post-title-${key}`}>
-                {/* eslint-disable jsx-a11y/anchor-is-valid */}
-                <Link
-                  className={`tst-post-link-${key}`}
-                  skin={constants.SKINS.default}
-                  link={`/post/${id}`}
-                >
-                  {title}
-                </Link>
-                {/* eslint-enable jsx-a11y/anchor-is-valid */}
-                {editButton}
-              </div>
-              <Image
-                className="gm-margin-center"
-                src={image}
-                height={200}
-                width={300}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+            return (
+              <FlexItem
+                key={`post-${id}`}
+                className={`gm-spacing-l tst-post-${key}`}
+              >
+                <Card isGradient>
+                  <Panel>
+                    <PanelTitle className={`tst-post-title-${key}`}>
+                      <Flex>
+                        <FlexItem isGrow>
+                          {/* eslint-disable jsx-a11y/anchor-is-valid */}
+                          <Link
+                            className={`tst-post-link-${key}`}
+                            skin={constants.SKIN_DEFAULT}
+                            link={`/post/${id}`}
+                          >
+                            {title}
+                          </Link>
+                          {/* eslint-enable jsx-a11y/anchor-is-valid */}
+                        </FlexItem>
+                        <FlexItem isShrink>{editButton}</FlexItem>
+                      </Flex>
+                    </PanelTitle>
+                    <PanelContent>
+                      <Image
+                        className="gm-margin-center"
+                        src={image}
+                        height={200}
+                        width={300}
+                      />
+                    </PanelContent>
+                  </Panel>
+                </Card>
+              </FlexItem>
+            );
+          })}
+        </Flex>
+      </PanelContent>
+    </Panel>
   );
 }
 
