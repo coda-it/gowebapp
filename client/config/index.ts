14db2e29a945da6d528d6cf404b7872ea2b6fafe
Oskar Szura <oskar.szura@gmail.com>
2024-12-04 08:50:35 +0100

    CODA-352 - Feature flags

--- client/config/index.ts
+++ client/config/index.ts
@@ -5,6 +5,8 @@ const platformConfig: config.Config = {
   ...window.config,
   // @ts-ignore - its a hack to load platform translations from the application template
   translation: window.translation,
+  // @ts-ignore - its a hack to load platform feature flags from the application template
+  featureFlags: window.featureFlags,
 };
 
 // @ts-ignore - its a hack to load platform config from the application template
