b82fb07b4a40c1f9e73f5faa45e384caa6bfe8c6
Oskar Szura <oskar.szura@gmail.com>
2025-12-28 20:12:29 +0100

    CODA-354 - Single container Azure migration

--- docker/webapp/prod/entrypoint.sh
+++ docker/webapp/prod/entrypoint.sh
@@ -34,6 +34,7 @@ echo "=== Starting app ==="
 APP=/root/go/src/github.com/coda-it/gowebapp
 
 rm -rf "$APP/public" "$APP/views" "$APP/translations"
+ln -sfn /home/volume/config        /root/volume
 ln -sfn /home/volume/public        "$APP/public"
 ln -sfn /home/volume/views         "$APP/views"
 ln -sfn /home/volume/translations  "$APP/translations"
