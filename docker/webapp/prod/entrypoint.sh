7721e1681193c8f40cfcbaa78c0ccc7f925fc5d7
Oskar Szura <oskar.szura@gmail.com>
2025-12-29 10:10:57 +0100

    CODA-354 - Single container Azure migration

--- docker/webapp/prod/entrypoint.sh
+++ docker/webapp/prod/entrypoint.sh
@@ -1,43 +1,11 @@
-echo "Waiting for mounts..."
-i=0
-while [ $i -lt 30 ]; do
-  [ -d /home/volume ] && [ "$(ls -A /home/volume 2>/dev/null || true)" ] && break
-  i=$((i+1))
-  sleep 1
-done
-
-echo "=== ENV (storage-related) ==="
-env | grep -E 'WEBSITE_|APPSETTING_|AZURE' || true
-
-echo
-echo "=== Mounts (filtered) ==="
-# pokaże tylko /home/volume i ewentualnie cifs/fuse
-(mount | grep -E '/home/volume| type cifs | type fuse' || true)
-
-echo
-echo "=== df -h (filtered) ==="
-(df -h | grep -E '/home/volume|Filesystem' || true)
-
-echo
-echo "=== Listing /home/volume ==="
-ls -la /home/volume || true
-
-for d in /home/volume/config /home/volume/public /home/volume/translations /home/volume/views; do
-  echo
-  echo "=== $d ==="
-  ls -la "$d" || echo "WARN: $d not accessible"
-done
-
-echo
-echo "=== Starting app ==="
-
+echo "=== Creating mount symlinks ==="
 APP=/root/go/src/github.com/coda-it/gowebapp
-
 rm -rf "$APP/public" "$APP/views" "$APP/translations"
 ln -sfn /home/volume/config        /root/volume
 ln -sfn /home/volume/public        "$APP/public"
 ln -sfn /home/volume/views         "$APP/views"
 ln -sfn /home/volume/translations  "$APP/translations"
 
+echo "=== Starting app ==="
 cd "$APP"
 exec ./gowebapp
